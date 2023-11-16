3db24ceaa602cf006cf23d1d19697f24d9ff374c
deathstroke-Assassin <moh2004amed@gmail.com>
2023-11-16 11:15:40 -0800

    h

--- execute.c
+++ execute.c
@@ -1,40 +1,89 @@
-#include <sys/wait.h>
 #include "shell.h"
 
 /**
- * execute_command - the executer
- * @command: the command
+ * handle_builtin - Handle Builtin Command
+ * @cmd: Parsed Command
+ * @er:statue of last Excute
+ * Return: -1 Fail 0 Succes (Return :Excute Builtin)
  */
-void execute_command(const char *command)
+
+int handle_builtin(char **cmd, int er)
 {
-	pid_t child_pid = fork();
+	 builtin bil[] = {
+		{"cd", change_dir},
+		{"env", dis_env},
+		{"help", display_help},
+		{"echo", echo_bul},
+		{"history", history_dis},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-	if (child_pid == -1)
+	while ((bil + i)->command)
 	{
-		kprint("Error forking process.\n");
-		exit(EXIT_FAILURE);
-	}
-	else if (child_pid == 0)
-	{
-		char *args[128];
-		char *env[] = {"/bin", NULL};
-		int arg_count = 0;
-		char *token = strtok((char *)command, " ");
-
-		while (token != NULL)
+		if (_strcmp(cmd[0], (bil + i)->command) == 0)
 		{
-			args[arg_count++] = token;
-			token = strtok(NULL, " ");
+			return ((bil + i)->fun(cmd, er));
 		}
-		args[arg_count] = NULL;
-
-		execve(args[0], args, env);
-
-		kprint("Error executing command.\n");
-		exit(EXIT_FAILURE);
+		i++;
 	}
-	else
+	return (-1);
+}
+/**
+ * check_cmd - Excute Simple Shell Command (Fork,Wait,Excute)
+ *
+ * @cmd:Parsed Command
+ * @input: User Input
+ * @c:Shell Excution Time Case of Command Not Found
+ * @argv:Program Name
+ * Return: 1 Case Command Null -1 Wrong Command 0 Command Excuted
+ */
+int check_cmd(char **cmd, char *input, int c, char **argv)
+{
+	int status;
+	pid_t pid;
+
+	if (*cmd == NULL)
 	{
-		wait(NULL);
+		return (-1);
+	}
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Error");
+		return (-1);
+	}
+
+	if (pid == 0)
+	{
+		if (_strncmp(*cmd, "./", 2) != 0 && _strncmp(*cmd, "/", 1) != 0)
+		{
+			path_cmd(cmd);
+		}
+
+		if (execve(*cmd, cmd, environ) == -1)
+		{
+			print_error(cmd[0], c, argv);
+			free(input);
+			free(cmd);
+			exit(EXIT_FAILURE);
+		}
+		return (EXIT_SUCCESS);
+	}
+	wait(&status);
+	return (0);
+}
+/**
+ * signal_to_handel - Handle ^C
+ * @sig:Captured Signal
+ * Return: Void
+ */
+void signal_to_handel(int sig)
+{
+	if (sig == SIGINT)
+	{
+		PRINTER("\n$ ");
 	}
 }
+
--- prompt.c
+++
@@ -1,8 +0,0 @@
-#include "shell.h"
-/**
- * display_prompt - the on screen prompt
- */
-void display_prompt(void)
-{
-kprint("khayat_shell$ ");
-}
